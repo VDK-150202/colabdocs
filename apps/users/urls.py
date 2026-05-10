d5b52a8d337bd412ca451fa7b2bfa51efa228314
vaibhav korlekar <vaibhavkorlekar3@example.com>
2026-05-10 23:07:57 +0530

    user reg changes

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import LoginView, LogoutView, RegisterView, UserProfileView
 
 urlpatterns = [
+    path("", RegisterView.as_view(), name="auth-register"),
     path("register/", RegisterView.as_view(), name="auth-register"),
     path("login/", LoginView.as_view(), name="auth-login"),
     path("logout/", LogoutView.as_view(), name="auth-logout"),
